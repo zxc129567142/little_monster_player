79078570141d1ec62875f6e4d1c251f8a2f242ef
zxc129567142 <bill61804@gmail.com>
2021-08-08 21:05:30 +0800

    增加音樂資料

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./main.css"
 
-console.log("Hello World!");
+// console.log("Hello World!");
+
+const player_cd = document.querySelectorAll(".player_cd")
+
+let cd_pre = null
+
+for (const cd of player_cd) {
+    cd.onclick = (e) => {
+        const _this = e.target
+        if (cd_pre) cd_pre.classList.remove("cd_center")
+        _this.classList.add("cd_center")
+        cd_pre = _this
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      height: ['hover'],
+    },
   },
   plugins: [],
 }
