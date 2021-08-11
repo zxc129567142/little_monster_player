8c843df6f33bb9acdb177a70a057aa220361d258
zxc129567142 <bill61804@gmail.com>
2021-08-11 23:37:34 +0800

    調整中心計算方式

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import "./main.css"
 
 // console.log("Hello World!");
 
+const getPVal = (dom,val) => window.getComputedStyle(dom).getPropertyValue(val)
+const getRootVar = v => (typeof v === "string") ? getPVal(document.documentElement, '--' + v).trim() : "";
+
+const calcWidthDiff = () => {
+    const cd_center = document.querySelector(".player_cd.cd_center");
+    const cd = document.querySelector(".player_cd:not(.cd_center)");
+    const maxW = getPVal(cd_center,"width").replace(/\D+/, "");
+    const minW = getPVal(cd, "width").replace(/\D+/, "");
+    console.log("maxW: " + maxW,"minW: " + minW);
+    return (maxW - minW)
+}
+
+const getFS = getPVal(document.body,"font-size").replace(/\D+/, "")
+
 const player_list = document.querySelector("#player_list > div")
 const list_scroll = document.querySelector("#player_list_scroll")
 const player_cd = document.querySelectorAll(".player_cd")
 
+const widthDiff = calcWidthDiff();
+console.log("widthDiff: " + widthDiff);
 const center = player_list.clientWidth / 2
-const scroll_center = list_scroll.clientWidth / 2
+const scroll_width = list_scroll.clientWidth / 2
 // console.log(center);
 // player_list.getBoundingClientRect()
 
 let cd_pre = document.querySelector(".player_cd.cd_center")
+
+let scroll_left = 0
+
 
 for (const cd of player_cd) {
     cd.onclick = (e) => {
@@ -22,10 +41,15 @@ for (const cd of player_cd) {
         const rects = _this.getBoundingClientRect()
         console.log(rects);
         const leftToCenter = rects.left + rects.width / 2;
-        const temp1 = ~~(rects.left - (leftToCenter - center))
+        const leftToCenterDiff = rects.left + (rects.width + widthDiff) / 2;
+        console.log("rects.left: " + rects.left);
+        console.log("rects.width: " + rects.width);
+        // scroll_left = ~~(rects.left - (leftToCenter - center))
+
+        scroll_left = scroll_left + ~(leftToCenterDiff - center )
         // const temp2 = temp1 + ~(temp1 - leftToCenter)
-        list_scroll.style.left = `${ temp1 }px`
-        console.log(leftToCenter,leftToCenter - center,rects.left,temp1);
+        list_scroll.style.left = `${ scroll_left }px`
+        console.log(leftToCenterDiff, leftToCenterDiff - center);
         cd_pre = _this
     }
 }
