9ee60fbff9cdac1d0cbb2489ab04b22034ffd881
zxc129567142 <bill61804@gmail.com>
2021-08-15 23:24:33 +0800

    增加可控制撥放器部分的功能˙

--- src/index.js
+++ src/index.js
@@ -1,20 +1,64 @@
-// import 'regenerator-runtime/runtime'
 import { mPost, mGet, getPVal, getRootVar } from './func.js';
 import Player from './player.js';
 import "./main.css";
-// import image from './audio/13i0STTSPqI.jpg'
 
-// console.log(image);
-
-// const player_control_panel = document.querySelector("#player_control_panel");
 const list_div = document.querySelector("#player_list > div");
-// const list_scroll = player_list.querySelector("#player_list_scroll");
-// const playlist = CD_DATA.playlist
 
-// const eventCD = () => {
+const control = document.querySelector("#player_control_panel");
+const play = control.querySelector("#play");
+const pause = control.querySelector("#pause");
 
-// }
-
-const player = new Player(list_div, document.querySelector("#player_control_panel"));
+const player = new Player(list_div, control);
 player.init()
 window.player = player
+
+// const progress_bar = control.querySelector(".progress_bar_main")
+const skip_previous = control.querySelector("#skip_previous")
+const play_and_pause = control.querySelector("#play_and_pause")
+const skip_next = control.querySelector("#skip_next")
+const volume = control.querySelector("#volume")
+
+skip_next.onclick = () => {
+    player.next();
+}
+
+skip_previous.onclick = () => {
+    player.previous()
+}
+
+play_and_pause.onclick = () => {
+    switch (player.state) {
+        case "playing":
+            play.style.display = "block";
+            pause.style.display = "none";
+            player.pause();
+            break;
+        case "pause":
+            play.style.display = "none";
+            pause.style.display = "block";
+            player.play();
+            break;
+        default:
+            break;
+    }
+    // if (player.state !== "playing") player.play();
+    // else player.pause();
+
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code
+window.onkeyup = (e) => {
+    switch (e.code) {
+        case "ArrowLeft":
+            player.previous()
+            break;
+        case "ArrowRight":
+            player.next();
+            break;
+
+        default:
+            break;
+    }
+}
+
+// console.log(player.volume);
--- src/player.js
+++ src/player.js
@@ -9,32 +9,45 @@ const playlist = data.playlist;
 
 export default class Player {
     constructor(list_div, control) {
-        this.data = data;
         this.list_div = list_div;
         this.control = control;
+        this.prog_bar_bg = control.querySelector(".progress_bar_bg");
+        this.prog_bar_main = control.querySelector(".progress_bar_main");
+
+        this.data = data;
+        this.cd_list = this.list_div.querySelector("#player_list_scroll");
+        this.cd_list.innerHTML = "";
+        this.cds = null;
+        this.cd_pre = null;
+        this.min = 0; // cd 最小位置
+        this.now = -1; // cd 當前位置
+        this.max = -1; // cd 最大位置
+        this.vol = 50;
+        this.state = "none"
+
+        this.audio = null;
+    }
+    mult_import(r) {
+        // 載入，須配合 webpack 設定
+        let mult = {};
+        r.keys().forEach(item => { mult[item.replace('./', '')] = r(item).default;});
+        return mult
     }
     init_list() {
         // 載入列表
-        const import_images = (r) => {
-            // 載入多圖，須配合 webpack 設定
-            let images = {};
-            r.keys().forEach(item => { images[item.replace('./', '')] = r(item).default;});
-            return images
-        }
-        const images = import_images(require.context('./audio', false, /\.jpg$/));
+        const images = this.mult_import(require.context('./audio', false, /\.jpg$/));
 
         let html = '';
-        // console.log(playlist["cIUrS5w9NbM"]);
         for (const id in playlist) {
             if (!Object.hasOwnProperty.call(playlist, id)) continue;
             const item = playlist[id];
             if (!item.enabled) continue;
             html += `<div class="center_line player_cd bg_img" data-id="${item.id}" style="background-image: url('${images[item.id + ".jpg"]}');"></div>`
+            this.max += 1
         }
 
         this.cd_list.insertAdjacentHTML("beforeend", html);
         this.cds = this.cd_list.querySelectorAll(".player_cd");
-        this.max = playlist.length
     }
     init_list_event() {
         // 載入列表後給 CD 加上事件
@@ -59,57 +72,140 @@ export default class Player {
 
         // let cd_pre = this.cd_list.querySelector(".player_cd.cd_center");
 
-        for (const cd of this.cds) {
+        // const mult_import = (r) => {
+        //     // 載入多圖，須配合 webpack 設定
+        //     let images = {};
+        //     r.keys().forEach(item => { images[item.replace('./', '')] = r(item).default;});
+        //     return images
+        // }
+        // const images = mult_import(require.context('./audio', false, /\.jpg$/));
+
+
+        const sources = this.mult_import(require.context('./audio', false, /\.m4a$/));
+
+        this.cds.forEach((cd,idx) => {
             cd.onclick = (e) => {
                 const _this = e.target;
                 if (this.cd_pre === _this) return;
                 if (this.cd_pre) this.cd_pre.classList.remove("cd_center");
                 _this.classList.add("cd_center");
+                this.pause();
+
+                // console.log("idx",this.now);
 
                 const vID = _this.dataset.id
-                console.log(playlist[vID]);
+                // const file_name = vID + ".m4a"
+                // console.log(playlist[vID]);
+                this.audio.src = sources[vID + ".m4a"];
 
-                const rects = _this.getBoundingClientRect()
-                console.log(rects);
+                this.now = idx;
+
+                const rects = _this.getBoundingClientRect();
                 const leftToCenter = rects.left + rects.width / 2;
                 // const leftToCenterDiff = rects.left + (rects.width + widthDiff) / 2;
 
                 scroll_left = scroll_left + ~(leftToCenter - center )
                 this.cd_list.style.left = `${ scroll_left }px`
-                console.log(leftToCenter,center, leftToCenter - center);
                 this.cd_pre = _this
+
+                // this.play();
             }
-        }
+        })
+
+    }
+    init_audio() {
+        let audio = new Audio();
+        audio.loop = false;
+
+        audio.type = "audio/m4a"
+        audio.volume = this.vol / 100 // 0.0 ~ 1.0
+        audio.addEventListener("canplaythrough", e => {
+            console.log("[canplaythrough]",this.audio.duration);
+            this.play();
+        });
+
+        audio.addEventListener('loadstart', ()=> {
+            console.log("[loadstart]");
+            this.progress_bar(0)
+        });
+
+        audio.addEventListener('progress', ()=> {
+            console.log("[progress]");
+        });
+
+        audio.addEventListener("canplay", () => {
+            console.log("[canplay]");
+        });
+
+        audio.addEventListener('timeupdate', () => {
+            // console.log("[timeupdate]",e,audio.currentTime);
+            // console.log("[timeupdate]", audio.currentTime);
+            this.progress_bar(this.audio.currentTime)
+        });
+
+        audio.addEventListener("readyState", () => {
+            console.log("[readyState]");
+        });
+
+        audio.addEventListener('ended', () => {
+            console.log('[ended]', this.audio.ended);
+            if (this.audio.ended) return this.next();
+        });
+
+        this.audio = audio;
     }
     // 初始化
     init() {
-        this.cd_list = this.list_div.querySelector("#player_list_scroll");
-        this.cd_list.innerHTML = "";
-        this.cds = null;
-        this.cd_pre = null;
-        this.min = 0; // cd 最小位置
-        this.now = -1; // cd 當前位置
-        this.max = 0; // cd 最大位置
+        this.init_audio();
         this.init_list();
         this.init_list_event();
+        this.next();
+    }
+    // 競渡條
+    progress_bar(value) {
+        value = (typeof value !== "number") ? 0 : value
+        const max = this.audio.duration
+        // value =
+        let pass = value / max * 100
+        pass = (pass >= 100) ? 100 : pass
+        // this.prog_bar_bg
+        this.prog_bar_main.style.width = pass + "%"
+
     }
     // 播放
-    play() { }
+    play() {
+        if (this.state === "playing") return;
+        this.state = "playing";
+        this.audio.play();
+    }
     // 暫停
-    pause() { }
+    pause() {
+        if (this.state === "pause") return;
+        this.state = "pause";
+        this.audio.pause();
+    }
     // 下一首
     next() {
         this.now += 1
-        if (this.now > this.max) this.now = this.max
-        this.cd_pre = this.cds[now]
-        this.cd_pre.click();
+        if (this.now > this.max) return this.now = this.max;
+        this.cds[this.now].click();
     }
     // 上一首
     previous() {
         this.now -= 1
-        if (this.now < this.min) this.now = this.min
-        this.cd_pre = this.cds[now]
-        this.cd_pre.click();
+        if (this.now < this.min) return this.now = this.min;
+        this.cds[this.now].click();
     }
+    // 音量
+    volume(val) {
+        if (typeof val !== "number") return;
+        if (val < 0) this.audio.volume = 0
+        if (val > 100) this.audio.volume = 100
+        this.audio.volume = val / 100;
+    }
+
+    // get state() { return this.state }
+    // get volume() { return this.vol }
+    get now_play() { return playlist[(this.cds[this.now].dataset.id)] }
 }
 
